30c3104a9eb43d24aabe00be4ac1afacdb173832
Adrian Cruz <kangoonie@gmail.com>
2025-12-11 00:42:18 +0100

    sort results

--- src/app/search/[value]/route.ts
+++ src/app/search/[value]/route.ts
@@ -11,20 +11,29 @@ export async function GET(req: NextRequest, context: { params: Promise<{ value: 
   const lowerCaseValue = value.toLowerCase()
 
   // Validate the request parameters
-  if (!value || value.length < 3)
+  if (!value || value.length < 3) {
+    console.error(`Error: Malformed query: ${value}`)
     return NextResponse.json(
       {
         error: 'Search must be at least 3 characters'
       },
       { status: 400 }
     )
+  }
 
+  const normalizedSearch = normalizeLetters(lowerCaseValue)
   const results = trie.search(lowerCaseValue, (accumulator, phrase, matches) => {
     return matches
-      .filter((x) => x.street.toLowerCase().startsWith(lowerCaseValue))
+      .filter((x) => normalizeLetters(x.street.toLowerCase()).startsWith(normalizedSearch))
       .sort((a, b) => {
-        return a.street.length - b.street.length
+        return a.street.localeCompare(b.street, 'no-NO')
       })
   })
+
   return NextResponse.json(results.slice(0, 20))
+}
+
+function normalizeLetters(value: string) {
+  // First: Remove standard diacritics using NFD
+  return value.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
 }
