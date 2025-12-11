af75f7a070c8e566a3d9f4add300fba92633f066
Adrian Cruz <kangoonie@gmail.com>
2025-12-11 09:45:51 +0100

    normalized search

--- src/app/lib/trie.ts
+++ src/app/lib/trie.ts
@@ -2,12 +2,36 @@ import TrieSearch from 'trie-search'
 import addresses from '@/data/adresses.json'
 import { Address } from '@/app/types'
 
-let trie: TrieSearch<Address> | null = null;
+type TrieNode = Address & { matchKey: string }
+let trie: TrieSearch<TrieNode> | null = null
 
 export function getTrie() {
   if (!trie) {
-    trie = new TrieSearch<Address>(['street'], { min: 3 })
-    trie.addAll(addresses as Address[])
+    trie = new TrieSearch<TrieNode>(['matchKey'], { min: 3 })
+    trie.addAll(
+      (addresses as Address[]).map((obj) => {
+        return { ...obj, matchKey: normalizeString(obj.street) }
+      })
+    )
   }
-  return trie;
+  return trie
+}
+
+export function normalizeString(str: string) {
+  return str
+    .toLowerCase()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(
+      /[\u0300-\u036f]|[æÆøØåÅ]/g,
+      (c) =>
+        ({
+          æ: 'ae',
+          Æ: 'Ae',
+          ø: 'o',
+          Ø: 'O',
+          å: 'a',
+          Å: 'A'
+        }[c] || '')
+    )
 }
--- src/app/search/[value]/route.ts
+++ src/app/search/[value]/route.ts
@@ -1,9 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { getTrie } from '@/app/lib/trie'
+import { getTrie, normalizeString } from '@/app/lib/trie'
 
 export async function GET(req: NextRequest, context: { params: Promise<{ value: string }> }) {
   const { value } = await context.params
-  const lowerCaseValue = value.toLowerCase()
 
   // Validate the request parameters
   if (!value || value.length < 3) {
@@ -16,19 +15,14 @@ export async function GET(req: NextRequest, context: { params: Promise<{ value: 
     )
   }
 
-  const normalizedSearch = normalizeLetters(lowerCaseValue)
-  const results = getTrie().search(lowerCaseValue, (accumulator, phrase, matches) => {
+  const normalizedSearch = normalizeString(value)
+  const results = getTrie().search(normalizedSearch, (accumulator, phrase, matches) => {
     return matches
-      .filter((x) => normalizeLetters(x.street.toLowerCase()).startsWith(normalizedSearch))
+      .filter((x) => x.matchKey.startsWith(normalizedSearch))
       .sort((a, b) => {
         return a.street.localeCompare(b.street, 'no-NO')
       })
   })
 
   return NextResponse.json(results.slice(0, 20))
-}
-
-function normalizeLetters(value: string) {
-  // First: Remove standard diacritics using NFD
-  return value.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
 }
